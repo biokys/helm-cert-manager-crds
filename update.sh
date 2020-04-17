b1cf58930d21aedd4de6608c3cf1d5b76ca97dc4
Ruben Vermeersch <ruben@rocketeer.be>
2020-04-17 13:33:49 +0200

    Create commit before tagging

--- update.sh
+++ update.sh
@@ -7,6 +7,8 @@ if [ -z "$version" ]; then
 fi
 
 curl "https://raw.githubusercontent.com/jetstack/cert-manager/release-$version/deploy/manifests/00-crds.yaml" > cert-manager-crds/templates/crds.yaml
+git add .
+git commit -a -m "Update CRDs to version $version"
 git tag "$version"
 
 echo "Done!"
